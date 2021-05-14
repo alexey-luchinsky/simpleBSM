7d6b48fdc2cb535dd89e41721b4e39419f967276
Aleksei Luchinskii <aluchi@bgsu.edu>
2021-05-14 09:00:58 -0400

    Temperature is shown for each room

--- app.R
+++ app.R
@@ -19,52 +19,69 @@ library(plotly)
 all_df = read.csv("all_df.csv")
 days = unique(all_df$dayN)
 
+######### Summary Panel #############
+summary_panel <- function() {
+    fluidPage(
+        
+        # Application title
+        titlePanel("Simple Building Management System"),
+        
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput("current_day",  "Current day:", 
+                            min = min(days), max = max(days),  value = max(days), step=1),
+                selectInput("metric", label = h3("Select"), 
+                            choices = c("Temperature", "Pressure", "Humidity"),
+                            selected = "Temperature")
+            ),
+            
+            # Show a plot of the generated distribution
+            mainPanel(
+                fluidRow(
+                    column(4, verbatimTextOutput("tempGauge")),
+                    column(4, verbatimTextOutput("presGauge")),
+                    column(4, verbatimTextOutput("humGauge"))
+                ),
+                plotOutput("historyPlot")
+            )
+        )
+    )    
+}
+
+#### Details Panel
+details_panel <- function() {
+    fluidPage(
+        titlePanel("Simple Building Management System"),
+        sliderInput("current_day2",  "Current day:", min = min(days), max = max(days),  value = max(days), step=1),
+        h2("Temperature"),
+        dataTableOutput('table'),
+        h2("Pressure"),
+        h2("Humidity")
+        
+    )
+}
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- tabsetPanel(position = "below",
+                  tabPanel("Summary", summary_panel()),
+                  tabPanel("Details", details_panel())
+                  )
 
-    # Application title
-    titlePanel("Simple Building Management System"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("current_day",  "Current day:", 
-                min = min(days), max = max(days),  value = max(days), step=1),
-            selectInput("metric", label = h3("Select"), 
-                        choices = c("Temperature", "Pressure", "Humidity"),
-                        selected = "Temperature")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           fluidRow(
-               column(4, verbatimTextOutput("tempGauge")),
-               column(4, verbatimTextOutput("presGauge")),
-               column(4, verbatimTextOutput("humGauge"))
-           ),
-           plotOutput("historyPlot")
-        )
-    )
-)
+gen_gauge <- function(input, output, metric) {
+    renderText({
+        current_day = input$current_day
+        all_df %>% select(dayN, y = tolower(metric)) %>% filter(dayN == current_day) %>% 
+            summarise(my = mean(y)) %>% .$my %>% round %>% paste(metric,.)
+    })
+}
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$tempGauge <- renderText({
-        current_day = input$current_day
-        all_df %>% select(dayN, temperature) %>% filter(dayN == current_day) %>% 
-            summarise(y = mean(temperature)) %>% .$y %>% round %>% paste("Temperature:",.)
-    })
-    output$presGauge <- renderText({
-        current_day = input$current_day
-        all_df %>% select(dayN, pressure) %>% filter(dayN == current_day) %>% 
-            summarise(y = mean(pressure)) %>% .$y %>% round %>% paste("Pressure:",.)
-    })
-    output$humGauge <- renderText({
-        current_day = input$current_day
-        all_df %>% select(dayN, humidity) %>% filter(dayN == current_day) %>% 
-            summarise(y = mean(humidity)) %>% .$y %>% round %>% paste("Humidity:",.)
-    })
+    output$tempGauge <- gen_gauge(input, output, "Temperature")
+    output$presGauge <- gen_gauge(input, output, "Pressure")
+    output$humGauge <- gen_gauge(input, output, "Humidity")
+    
     output$historyPlot <- renderPlot({
         current_day = input$current_day
         metric = input$metric
@@ -73,6 +90,14 @@ server <- function(input, output) {
             geom_vline(xintercept = current_day, linetype = "dashed") +
             labs(x = "Day", y = metric, title = paste("Mean", tolower(metric)))
     })
+    
+
+    output$table <- renderDataTable({
+        current_day = input$current_day2
+        df <- all_df %>% filter(dayN == current_day) %>% select(room, temperature) %>% round
+        df
+        })
+
 }
 
 # Run the application 
